5306681b6b9959cbab08a438bb62a952f94c5438
Kelvin Njihia <kelvin2012kim@gmail.com>
2020-07-24 15:21:43 +0200

    Dropped old components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent
+    path: '',
+    component: AppComponent
   },
   {
     path: '',
